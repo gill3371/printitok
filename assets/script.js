d31f3ade7becbc81ad23ccc7bd40a0fc68b1a342
gill3371 <gilles.bailly@agencealchimie.fr>
2023-07-19 13:12:26 +0200

    ajout_script_flèches_et_points

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,45 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let baliseArrowLeft = document.querySelector(".arrow_left");
+baliseArrowLeft.onclick = () => {
+	if (idFleche === 0){
+		idFleche = (slides.length - 1);
+	} else {
+		idFleche--;
+	}
+	console.log(idFleche);
+	insertSlider(idFleche,slides);
+	bullet();
+}
+
+let baliseArrowRight = document.querySelector(".arrow_right");
+baliseArrowRight.onclick = () => {
+	if (idFleche === (slides.length - 1)){
+		idFleche = 0;
+	} else {
+		idFleche++;
+	}
+	console.log(idFleche);
+	insertSlider(idFleche,slides);
+	bullet();
+}
+
+// Affiches des points slider selon le nombre de diapo
+
+let baliseDivDots = document.querySelector(".dots");
+function bullet() {
+	let baliseADots;
+	baliseDivDots.innerHTML = '';
+	for (i=0; i<slides.length; i++) {
+		if (i === idFleche) {
+			baliseADots = '<a href="#" class="dot dot_selected"></a>';
+		} else {
+			baliseADots = '<a href="#" class="dot"></a>';
+		}
+		baliseDivDots.innerHTML += baliseADots;
+	}
+}
+
+bullet();
